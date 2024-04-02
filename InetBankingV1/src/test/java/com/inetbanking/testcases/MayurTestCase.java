8409e16de3a84c8b6959a631e026d103b8043824
mayur <mayur@LAPTOP-VIJQ9PPK>
2024-04-02 19:11:30 +0530

    Hi This is My New Test Case

---
+++ InetBankingV1/src/test/java/com/inetbanking/testcases/MayurTestCase.java
@@ -0,0 +1,10 @@
+package com.inetbanking.testcases;
+
+public class MayurTestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hi This is Mayur Test Case");
+	}
+
+}
