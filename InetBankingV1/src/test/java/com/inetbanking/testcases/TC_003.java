820d9b5a1050c66e2374a0a007656f4572fce3a6
TL <TL@gmail.com>
2024-04-02 20:19:26 +0530

    This my Code

---
+++ InetBankingV1/src/test/java/com/inetbanking/testcases/TC_003.java
@@ -0,0 +1,12 @@
+package com.inetbanking.testcases;
+
+public class TC_003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is My New Code");
+
+	}
+
+}
