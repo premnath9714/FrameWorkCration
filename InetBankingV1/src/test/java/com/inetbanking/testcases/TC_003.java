855aa1e103e15fa135a8698d99b809c23a8d8d1a
mayur <mayur@LAPTOP-VIJQ9PPK>
2024-04-02 20:23:31 +0530

    This changes in TC_0003

---
+++ InetBankingV1/src/test/java/com/inetbanking/testcases/TC_003.java
@@ -0,0 +1,12 @@
+package com.inetbanking.testcases;
+
+public class TC_003 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is My New Code");
+		System.out.println(" Hello New Mumbai");
+
+	}
+
+}
