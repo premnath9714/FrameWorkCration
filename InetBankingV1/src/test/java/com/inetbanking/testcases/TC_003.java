4ce3a47f799a52d2bd8119f056a0850ac45d8d49
mayur <mayur@LAPTOP-VIJQ9PPK>
2024-04-02 20:28:23 +0530

    This is New test cases TC_004

--- InetBankingV1/src/test/java/com/inetbanking/testcases/TC_003.java
+++ InetBankingV1/src/test/java/com/inetbanking/testcases/TC_003.java
@@ -5,6 +5,7 @@ public class TC_003 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is My New Code");
+		
 		System.out.println(" Hello New Mumbai");
 
 	}
---
+++ InetBankingV1/src/test/java/com/inetbanking/testcases/TC_004.java
@@ -0,0 +1,10 @@
+package com.inetbanking.testcases;
+
+public class TC_004 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(" My New TC_004");
+	}
+
+}
